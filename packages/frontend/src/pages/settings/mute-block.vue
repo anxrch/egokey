c0adedaf5d0730a8ef660e13a4252f5436e5aecc
vuivahn <308605102+vuivahn@users.noreply.github.com>
2026-08-01 23:00:50 +0900

    fix: tidy muted user visibility setting search metadata
    (cherry picked from commit bed2242b543d7c40cf15e81c75b13c3421bf6eff)

--- packages/frontend/src/pages/settings/mute-block.vue
+++ packages/frontend/src/pages/settings/mute-block.vue
@@ -114,10 +114,9 @@ SPDX-License-Identifier: AGPL-3.0-only
 						<MkSwitch v-model="deidentifyMutedUsers">{{ i18n.ts.deidentifyMutedUsers }}</MkSwitch>
 
 						<SearchMarker
-							:label="i18n.ts.hideMutedUsers"
 							:keywords="['mute', 'notification', 'reaction', 'hide', 'user']"
 						>
-							<MkSwitch :modelValue="$i.hideMutedUsers" @update:model-value="updateHideMutedUsers">
+							<MkSwitch :modelValue="$i.hideMutedUsers" @update:modelValue="updateHideMutedUsers">
 								<template #label><SearchLabel>{{ i18n.ts.hideMutedUsers }}</SearchLabel></template>
 								<template #caption><SearchText>{{ i18n.ts.hideMutedUsersDescription }}</SearchText></template>
 							</MkSwitch>
